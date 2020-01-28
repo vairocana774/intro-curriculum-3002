a4e4338a455566b5bcf2947491ef6de38293a035
vairocana774 <vairocana774@gmail.com>
2020-01-29 00:28:02 +0900

    都道府県人工ランキングの降順ソート実装完了

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+    'use strict';
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
@@ -33,10 +33,10 @@ rl.on('close', () => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key, value],i) => {
+        return (i + 1) + `位　${key}  ${value.popu10} -> ${value.popu15} 変化率：　${value.change}`
     });
     console.log(rankingStrings);
 });
